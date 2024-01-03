446eeea084da46b943059e4d76bd4eebf370cdc0
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-01-03 17:33:58 +0400

    updated env variabels

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -8,7 +8,7 @@ const Blog = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`;
+        const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`;
         const response = await fetch(url);
         const data = await response.json();
 
