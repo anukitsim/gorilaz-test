3ca82396ad53d557fdd1eb15f4516f24f80546e9
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-01-10 23:01:23 +0400

    og image

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -16,6 +16,10 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="XaVrDV0GoR7Pzjdl_2cyPfcCszKRnb-K4zSiOejky4k"
         />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
       </head>
       <body>{children}</body>
     </html>
