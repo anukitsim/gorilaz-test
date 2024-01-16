68542a9d558f7f3473b0a50f3df54ccbc423a403
anukitsim <104069419+anukitsim@users.noreply.github.com>
2024-01-16 13:48:48 +0400

    scrollbar fix

--- app/components/Video2.jsx
+++ app/components/Video2.jsx
@@ -1,11 +1,12 @@
-// Video2.jsx
+"use client"
+
 import { useRef, useState, useEffect } from "react";
 
 const Video2 = () => {
   const videoRef = useRef(null);
   const [videoUrl, setVideoUrl] = useState(null);
-  const [isVideo1Loaded, setIsVideo1Loaded] = useState(false);
-  const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_URL
+  const [isVideo2Loaded, setIsVideo2Loaded] = useState(false);
+  const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_URL;
 
   useEffect(() => {
     const fetchVideo2 = async () => {
@@ -19,7 +20,7 @@ const Video2 = () => {
 
           if (video2Url) {
             setVideoUrl(video2Url);
-            setIsVideo1Loaded(true);
+            setIsVideo2Loaded(true);
           } else {
             console.error("Video2 URL not found or invalid:", video2Url);
           }
@@ -50,6 +51,11 @@ const Video2 = () => {
         /iPad|iPhone|iPod/.test(navigator.userAgent);
 
       if (isSafari) {
+        // For Safari, use playsinline and autoplay
+        videoRef.current.setAttribute("playsinline", "");
+        videoRef.current.play();
+      } else {
+        // For other browsers, handle play/pause as before
         if (videoRef.current.paused) {
           videoRef.current.play();
         } else {
@@ -59,22 +65,23 @@ const Video2 = () => {
     }
   };
 
-  
-  return isVideo1Loaded ? (
-    <div  className="md:w-11/12  md:h-[560px] sm:w-[255.11px] sm:h-[172.83px] object-cover  text-white text-4xl flex justify-center items-center relative" >
-     <div className="absolute  md:h-[540px] top-[10px] bottom-[10px] left-[10px] right-[10px]   z-50 rounded-md border border-white"></div>
-    <video
-      ref={videoRef}
-      className="rounded-md z-0"
-      autoPlay
-      muted
-      loop
-      preload="metadata"
-      onClick={handleVideoClick}
-    />
+  return isVideo2Loaded ? (
+    <div className="md:w-11/12 md:h-[560px] sm:w-[255.11px] sm:h-[172.83px] object-cover text-white text-4xl flex justify-center items-center relative">
+      <div className="absolute md:h-[540px] top-[10px] bottom-[10px] left-[10px] right-[10px] z-50 rounded-md border border-white" onClick={handleVideoClick}></div>
+      <video
+        ref={videoRef}
+        className="rounded-md z-0"
+        autoPlay
+        muted
+        loop
+        preload="metadata"
+        
+      />
     </div>
   ) : (
-    <div  className="outline md:w-11/12 md:h-[520px] sm:w-[255.11px] sm:h-[172.83px] object-cover outline-white md:outline-offset-[-10px] sm:outline-offset-[-4px] md:rounded-[15px] sm:rounded-md text-white text-4xl flex justify-center items-center">Loading..</div>
+    <div className="outline md:w-11/12 md:h-[520px] sm:w-[255.11px] sm:h-[172.83px] object-cover outline-white md:outline-offset-[-10px] sm:outline-offset-[-4px] md:rounded-[15px] sm:rounded-md text-white text-4xl flex justify-center items-center">
+      Loading..
+    </div>
   );
 };
 
